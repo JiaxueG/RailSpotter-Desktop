a4c967e80929afcd4a9193127b656695f607c2fe
JiaxueG <62087221+JiaxueG@users.noreply.github.com>
2020-04-01 01:54:47 +0800

    20200331

--- launcher.py
+++ launcher.py
@@ -15,11 +15,12 @@ class launcher(QMainWindow, Ui_MainWindow):
         self.basicUi()
         # Controller
         self.ui.btnInput.clicked.connect(self.imageOpen)
+        self.ui.btnSave.clicked.connect(self.saveAll)
 
     def basicUi(self): 
         self.ui.status=self.statusBar()
         self.ui.status.showMessage('RailSpotter Desktop by JiaxueG v0.1')
-
+ 
     def imageOpen(self): 
         try: 
             self.imgName, imgType = QFileDialog.getOpenFileName(self, "打开图片", "", "*.jpg;;All Files(*)")
@@ -39,6 +40,40 @@ class launcher(QMainWindow, Ui_MainWindow):
                 scale = scaleHeight
         imageDisplay = QtGui.QPixmap(self.imgName).scaled(image.width()/scale, image.height()/scale)
         self.ui.labelPicview.setPixmap(imageDisplay)  # display
+    
+    def isEmpty(self): 
+        return self.length==0
+
+    def saveAll(self): #Incompleted
+        # copy everything to history
+        if self.ui.lineEditCountry.text(): # is not empty
+            self.ui.listWidgetCountry.addItem(self.ui.lineEditCountry.text())
+        if self.ui.lineEditLocation.text(): 
+            self.ui.listWidgetLocation.addItem(self.ui.lineEditLocation.text())
+        if self.ui.lineEditLoco.text(): 
+            self.ui.listWidgetLoco.addItem(self.ui.lineEditLoco.text())
+        if self.ui.lineEditNumber.text(): 
+            self.ui.listWidgetNumber.addItem(self.ui.lineEditNumber.text())
+        if self.ui.lineEditCarriage.text(): 
+            self.ui.listWidgetCarriage.addItem(self.ui.lineEditCarriage.text())
+        if self.ui.lineEditCompany.text(): 
+            self.ui.listWidgetCompany.addItem(self.ui.lineEditCompany.text())
+        if self.ui.lineEditLine.text()
+            self.ui.listWidgetLine.addItem(self.ui.lineEditLine.text())
+        if self.ui.lineEditService.text(): 
+            self.ui.listWidgetService.addItem(self.ui.lineEditService.text())
+        if self.ui.lineEditCamera.text(): 
+            self.ui.listWidgetCamera.addItem(self.ui.lineEditCamera.text())
+        if self.ui.lineEditLens.text(): 
+            self.ui.listWidgetLens.addItem(self.ui.lineEditLens.text())
+        if self.ui.lineEditMemo.text(): 
+            self.ui.listWidgetMemo.addItem(self.ui.lineEditMemo.text())
+        
+        
+
+
+        
+        # self.ui.listWidgetCountry.addItem(self.ui.lineEditCountry.text())
 
 if __name__=='__main__': 
     app=QApplication(sys.argv)
