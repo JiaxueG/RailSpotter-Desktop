a199dc091767a7fc679baf15227c60c65afe1b11
JiaxueG <62087221+JiaxueG@users.noreply.github.com>
2020-04-01 22:49:11 +0800

    20200401

--- launcher.py
+++ launcher.py
@@ -16,11 +16,12 @@ class launcher(QMainWindow, Ui_MainWindow):
         # Controller
         self.ui.btnInput.clicked.connect(self.imageOpen)
         self.ui.btnSave.clicked.connect(self.saveAll)
+        self.ui.btnMU.clicked.connect(self.setMU)
 
     def basicUi(self): 
         self.ui.status=self.statusBar()
         self.ui.status.showMessage('RailSpotter Desktop by JiaxueG v0.1')
- 
+         
     def imageOpen(self): 
         try: 
             self.imgName, imgType = QFileDialog.getOpenFileName(self, "打开图片", "", "*.jpg;;All Files(*)")
@@ -43,6 +44,19 @@ class launcher(QMainWindow, Ui_MainWindow):
     
     def isEmpty(self): 
         return self.length==0
+    
+    def setMU(self): 
+        if self.ui.checkBoxMU.isChecked()==False: 
+            self.ui.checkBoxMU.setCheckState(2)
+            self.ui.btnCarriage.setEnabled(False)
+            self.ui.lineEditCarriage.setEnabled(False)
+            self.ui.listWidgetCarriage.setEnabled(False)
+        else: 
+            self.ui.checkBoxMU.setCheckState(0)
+            self.ui.btnCarriage.setEnabled(True)
+            self.ui.lineEditCarriage.setEnabled(True)
+            self.ui.listWidgetCarriage.setEnabled(True)
+        
 
     def saveAll(self): #Incompleted
         # copy everything to history
@@ -58,7 +72,7 @@ class launcher(QMainWindow, Ui_MainWindow):
             self.ui.listWidgetCarriage.addItem(self.ui.lineEditCarriage.text())
         if self.ui.lineEditCompany.text(): 
             self.ui.listWidgetCompany.addItem(self.ui.lineEditCompany.text())
-        if self.ui.lineEditLine.text()
+        if self.ui.lineEditLine.text(): 
             self.ui.listWidgetLine.addItem(self.ui.lineEditLine.text())
         if self.ui.lineEditService.text(): 
             self.ui.listWidgetService.addItem(self.ui.lineEditService.text())
@@ -68,12 +82,12 @@ class launcher(QMainWindow, Ui_MainWindow):
             self.ui.listWidgetLens.addItem(self.ui.lineEditLens.text())
         if self.ui.lineEditMemo.text(): 
             self.ui.listWidgetMemo.addItem(self.ui.lineEditMemo.text())
-        
-        
+        # File system check
+        if self.ui.checkBoxFStime.isChecked()==True: 
+            # Save as file system => file
+        if self.ui.checkBoxFSvehicle.isChecked()==True: 
+            # Save as file system => vehicle
 
-
-        
-        # self.ui.listWidgetCountry.addItem(self.ui.lineEditCountry.text())
 
 if __name__=='__main__': 
     app=QApplication(sys.argv)
